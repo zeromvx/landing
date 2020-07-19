754155d9504f0e41641a495ff67ab098b539f3aa
zeromvx <zeromvx@gmail.com>
2020-07-19 11:24:17 +1000

    added features section

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ const navList = document.querySelector(".nav-list");
 const navbar = document.querySelector(".navbar");
 const toggleBtn = document.querySelector(".toggle-btn");
 const icon = document.querySelector(".toggle-icon");
-const header = document.querySelector(".header");
 
 toggleBtn.addEventListener("click", () => {
 	navList.classList.toggle("nav-list-active");
@@ -10,13 +9,13 @@ toggleBtn.addEventListener("click", () => {
 
 	if (navList.classList.contains("nav-list-active")) {
 		navList.style.height = "300px";
-		navbar.style.background = "#59687C";
 
 		for (let item of navList.children) {
 			item.style.opacity = 1;
 		}
 	} else {
 		navList.style.height = "0";
+
 		for (let item of navList.children) {
 			item.style.opacity = 0;
 		}
@@ -24,6 +23,7 @@ toggleBtn.addEventListener("click", () => {
 });
 
 document.addEventListener("scroll", () => {
+
 	if (pageYOffset > navbar.offsetHeight) {
 		navbar.style.background = "#59687C";
 		navbar.style.boxShadow =
